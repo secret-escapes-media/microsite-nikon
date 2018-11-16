f8c2f793e8f467db7270817bfad6ffea51ec1081
hamishgray <hamish.gray@secretescapes.com>
2018-11-16 17:42:14 +0000

    site build progress for design review

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,1 +1,17 @@
 // general js for the project that wouldn't be a reuseable component
+
+
+function bgFade(){
+  var st = $(document).scrollTop();
+  var wh = $(window).height();
+  var opacity = ((wh - (st*2)) / -wh)+1;
+
+  if(st<wh && opacity<0.9){
+  	$('.js-overlay-fade').css({
+  		"opacity": opacity
+  	});
+  }
+}
+
+bgFade();
+$(document).scroll(function(){ bgFade(); });
