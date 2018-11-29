5265d1f42af316c29f5f302b70f5b830f66da74d
hamishgray <hamish.gray@secretescapes.com>
2018-11-29 10:04:27 +0000

    amends, round 2 review

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -15,3 +15,43 @@ function bgFade(){
 
 bgFade();
 $(document).scroll(function(){ bgFade(); });
+
+
+$('.photo__content').click(function(){
+  var details = $(this).find('.photo__details');
+  if( details.hasClass('open') ){
+    details.removeClass('open').slideUp();
+  }else{
+    details.addClass('open').slideDown();
+  }
+});
+
+
+
+
+
+//## Singapore countdown
+//##########################
+
+$('.js-countdown').each(function() {
+  // gets the expires date from the object
+  var endDate = $(this).data('date');
+  $(this).countdown(endDate, function(event) {
+
+    if (event.offset.totalDays !== 0) {
+
+      $(this).find('.btn').hide();
+      $(this).removeClass('is-active').addClass('is-inactive');
+
+      $(this).find('.countdown__days').html(event.strftime('%D'));
+      $(this).find('.countdown__hours').html(event.strftime('%H'));
+      $(this).find('.countdown__minutes').html(event.strftime('%M'));
+      $(this).find('.countdown__seconds').html(event.strftime('%S'));
+    }else{
+      $(this).removeClass('is-inactive').addClass('is-active');
+      $(this).find('.story__countdown').hide();
+    }
+
+  });
+});
+
