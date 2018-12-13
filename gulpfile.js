9564dad01a265875287a81f144ec38baa81c1756
hamishgray <hamish.gray@secretescapes.com>
2018-12-13 14:34:15 +0000

    site published live

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('serve', ['build-jekyll'], function() {
     server: {
       baseDir: '_site/',
       routes: {
-        '/nikon-home': '_site/'
+        '/nikon': '_site/'
       }
     }
   });
